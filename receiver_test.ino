17c4d3c0532be9559e4c049ec18735e3c0e6a6c5
Zuhdan Ubay <ubayresearch@gmail.com>
2017-12-03 17:16:58 +0700

    add transmitter and receiver source code.

---
+++ receiver_test.ino
@@ -0,0 +1,116 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+RF24 radio(7, 8);
+const byte rxAddr[6] = "00001";
+boolean onConnection, authorize, handShaked;
+String data, dataHandShake;
+void setup(){
+  while (!Serial);
+  Serial.begin(9600);
+  Serial.println(F("Now is Debugging Mode with SysOut"));
+  radio.begin();
+  radio.setRetries(15, 15);
+  radio.setPALevel( RF24_PA_HIGH ) ;
+  clearData();
+  closeConnection();
+}
+void loop(){
+  while (!onConnection){
+    if (getSignal()){
+      if (data != ""){
+        Serial.println("Signal Received, Opening Connection from " + data);
+        onConnection = true;     
+      }
+    }
+  }
+  while (onConnection){
+    Serial.println("Doing Transaction with Services");
+    transaction();
+    Serial.println("Get Transaction Result, Send it To Door");
+    sendRespond(authorize);
+    Serial.println("Respond has Been Send, Clearing Data and Waiting for Connection");
+    clearData();
+    closeConnection();
+  }
+}
+boolean getSignal(){
+  boolean stat = false;
+  openListeningConnection();
+  if (radio.available())
+  {
+    char text[32] = {0};
+    radio.read(&text, sizeof(text));
+    data = String(text);
+    stat = true;
+  }
+  return stat;
+}
+void transaction(){
+  while(!Serial.available()){};
+  if (Serial.read() == 'a'){
+    authorize = true;
+  } else if (Serial.read() == 'b'){
+    authorize = false;
+  }
+}
+void sendRespond(boolean authorize){
+  int i = 0;
+  openWritingConnection();
+  while (i < 17){
+    if (authorize){
+      Serial.println(F("Authorizing..."));
+      const char text[] = "AccessGranted";
+      radio.write(&text, sizeof(text));
+    } else {
+      Serial.println(F("Denied..."));
+      const char text[] = "AccessGrantid";
+      radio.write(&text, sizeof(text));
+    }
+    i++;
+    delay(50);
+//    for (int i = 0; i < 20; i++){
+//      Serial.println("Waiting Hand Shake");
+//      handShakingReceive();
+//      delay(100);
+//    }
+  }
+}
+void handShakingReceive(){
+  openListeningConnection();
+  if (radio.available())
+  {
+    char text[32] = {0};
+    radio.read(&text, sizeof(text));
+    dataHandShake = String(text);
+    Serial.println("Data Hand Sake = " + dataHandShake);
+    if (dataHandShake == "ok"){
+      Serial.println("Hand Shaked!");
+      handShaked = true;
+      //handShakingSend();
+    }
+  }
+  delay(100);
+}
+void handShakingSend(){
+  openWritingConnection();
+  const char respond[] = "oks";
+  radio.write(&respond, sizeof(respond));
+}
+void openWritingConnection(){
+  radio.openWritingPipe(rxAddr);
+  radio.stopListening();
+}
+void openListeningConnection(){
+  radio.openReadingPipe(0, rxAddr);
+  radio.startListening();
+}
+void clearData(){
+  data = "";
+  dataHandShake = "";
+  authorize = false;
+  handShaked = false;
+}
+void closeConnection(){
+  onConnection = false;
+}
---
+++ transmitter_rfid.ino
@@ -0,0 +1,163 @@
+#include <EEPROM.h>
+#include <SPI.h>
+#include <MFRC522.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+MFRC522 mfrc522(10, 9);
+RF24 radio(7, 8);
+byte readCard[4];
+const byte rxAddr[6] = "00001";
+uint8_t successRead;
+boolean onConnection, authorize, result, handShaked, handShakingReceived;
+String data, readedCard, dataHandShake;
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Ready, Tap The Card!");
+  SPI.begin();
+  mfrc522.PCD_Init();
+  radio.begin();
+  radio.setRetries(15, 15);
+  radio.setPALevel( RF24_PA_HIGH ) ;
+  clearData();
+  closeConnection();
+}
+void loop () {
+  do {
+    successRead = getID();
+  } while (!successRead);
+  if (successRead){
+    Serial.println("ID Readed");
+    readCards();
+    Serial.println("ID Card is " + readedCard);
+    sendSignal(readedCard);
+    Serial.println("Getting Authorize, and Showing Result");
+    result = gettingAuthorize();
+    showResult(result);
+    clearData();
+  }
+}
+uint8_t getID() {
+  if ( ! mfrc522.PICC_IsNewCardPresent()) {
+    return 0;
+  }
+  if ( ! mfrc522.PICC_ReadCardSerial()) {
+    return 0;
+  }
+  mfrc522.PICC_HaltA();
+  return 1;
+}
+void readCards(){
+  for ( uint8_t i = 0; i < 4; i++) {
+    readCard[i] = mfrc522.uid.uidByte[i];
+    readedCard = String(readedCard + readCard[i]);
+  }
+}
+void sendSignal(String readedCards){
+  //todo, isSignalReceived
+  openWritingConnection();
+  String builder = "FirstDoor," + readedCards;
+  int builder_len = builder.length() + 1;
+  const char charBuilder[builder_len];
+  builder.toCharArray(charBuilder, builder_len);
+  radio.write(&charBuilder, sizeof(charBuilder));
+  Serial.println(charBuilder);
+  Serial.println("Signal has been Sent!");
+}
+boolean gettingAuthorize(){
+  boolean res;
+  Serial.println("Open Listening Connection");
+  openListeningConnection();
+  while (!onConnection){
+    if (getSignal()){
+      onConnection = true;  
+    }
+  }
+  while (onConnection){
+    res = transaction();
+    closeConnection();
+    clearData();
+  }
+  return res;
+}
+boolean getSignal(){
+  boolean stat = false;
+  openListeningConnection();
+  if (radio.available() > 0)
+  {
+    char text[32] = {0};
+    radio.read(&text, sizeof(text));
+    data = String(text);
+    Serial.println(data);
+    if (data != ""){
+      Serial.println("Signal Auth Received");
+      radio.stopListening();
+      stat = true;
+    }
+    //data == "AccessGranted" || data == "AccessDenied"
+  }
+  return stat;
+}
+boolean transaction(){
+  boolean res;
+  if (data == "AccessGranted"){
+    res = true;
+  } else if (data == "AccessGrantid"){
+    res = false;
+  }
+  return res;
+}
+void showResult(boolean result){
+   if (result){
+     Serial.println("------> DOOR UNLOCKED <------");
+   } else {
+     Serial.println("------> DOOR STILL LOCKED <------");
+   }
+}
+void handShaking(){
+  Serial.println("state handShakingReceived = " + handShakingReceived);
+  //while (!handShakingReceived){
+    openWritingConnection();
+    Serial.println("Sending handShaking OK 12 times");
+    for (int i = 0; i < 20; i++){
+      Serial.println("Sending Hand Sake");
+      const char respond[] = "ok";
+      radio.write(&respond, sizeof(respond));
+      delay (100);
+    }
+//    handShakingReceive();
+  //}
+}
+void handShakingReceive(){
+  openListeningConnection();
+  if (radio.available())
+  {
+    char text[32] = {0};
+    radio.read(&text, sizeof(text));
+    dataHandShake = String(text);
+    Serial.println("Data Hand Sake = " + dataHandShake);
+    if (dataHandShake == "oks"){
+      Serial.println("Hand Shaked!");
+      handShakingReceived = true;
+    }
+  }
+}
+void openWritingConnection(){
+  radio.openWritingPipe(rxAddr);
+  radio.stopListening();
+}
+void openListeningConnection(){
+  radio.openReadingPipe(0, rxAddr);
+  radio.startListening();
+}
+void clearData(){
+  readedCard = "";
+  successRead = 0;
+  data = "";
+  dataHandShake = "";
+  handShakingReceived = false;
+  authorize = false;
+}
+void closeConnection(){
+  onConnection = false;
+}
+
